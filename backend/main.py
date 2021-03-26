56589834b39440fa8d73a1fade8ec997430121e3
avinash-vk <avinash2000vk@gmail.com>
2021-03-26 23:52:32 +0530

    added dummy code for user

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
 import os
 
+import firebase_admin
+from firebase_admin import credentials
+cred = credentials.Certificate(os.getenv("CREDS_PATH"))
+firebase_admin.initialize_app(cred)
+
 from flask import Flask
 from dotenv import load_dotenv
 load_dotenv()
@@ -18,7 +23,22 @@ class APIServer:
         return 'pong'
 
     # Users endpoints
+
+    # create user api
+    @app.route('/createUser')
+    def createUser():
+        return "create user called"
     
+    # update user endpoint
+    @app.route('/updateUser')
+    def updateUser():
+        return "update user called"
+    
+    # get user endpoint
+    @app.route('/getUser')
+    def getUser():
+        return "get user called"
+
 
 server = APIServer(port = 5000)
 server.start()
