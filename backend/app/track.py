67f4131f3dbffff8034d28a88437516299a8c894
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2021-03-27 19:19:17 +0530

    Track class

---
+++ backend/app/track.py
@@ -0,0 +1,24 @@
+class Track(object):
+    def __init__(self,tname,artist,genre,desc,cover,mp3file):
+        self.tname=tname
+        self.artist=artist
+        self.genre=genre
+        self.desc=desc
+        self.cover=cover
+        self.mp3file=mp3file
+        
+    def modify_track(self):
+        pass
+
+    def get_track(self):
+        pass
+
+class TrackManager(object):
+    def __init__():
+        pass
+    def addNewTrack():
+        pass
+    def deleteTrack():
+        pass
+    def getTracks():
+        pass
