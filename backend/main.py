fcf6f1ec4c6dd30ebb19c5e22d22d080664b3413
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2021-03-28 14:13:59 +0530

    added Firestore upload

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ from firebase_admin import storage
 import os
 from werkzeug.utils import secure_filename
 upload_folder ="app\\uploads"
+import time
 
 cred = credentials.Certificate(os.getenv("CREDS_PATH"))
 firebase_admin.initialize_app(cred, {
@@ -20,7 +21,7 @@ bucket = storage.bucket()
 
 app = Flask(__name__)
 
-'''
+
 allowed_extensions_cover = {'png', 'jpg', 'jpeg'}
 def allowed_file_cover(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in allowed_extensions_cover
@@ -28,18 +29,23 @@ def allowed_file_cover(filename):
 allowed_extensions_track = {'mp3'}
 def allowed_file_track(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in allowed_extensions_track
-'''
+
 
 class FileUploader:
     def __init__(self):
         pass
     def uploaderimg(self,imagepath,cover):
-        print(imagepath)
-        blob = bucket.blob("cover/myfile.jpg")
+        blob = bucket.blob("cover/img"+str(time.time()))
         blob.upload_from_filename(
         imagepath
         )
-        print(blob.public_url)
+        return(blob.public_url)
+    def uploadertrack(self,trackpath,track):
+        blob = bucket.blob("tracks/trk"+str(time.time()))
+        blob.upload_from_filename(
+        trackpath
+        )
+        return(blob.public_url)
 
 class APIServer:
     def __init__(self,port):
@@ -75,15 +81,12 @@ class APIServer:
     def addTrack():
         if request.method == 'POST':
             try:
-                '''
                 tnm=request.form['tname']
                 artist=request.form['artist']
                 genre=request.form['genre']
                 desc=request.form['desc']
-                descrip=request.form['descrip']
-                '''
                 cover=request.files['cover']
-                #mp3file=request.files['mp3file']
+                mp3file=request.files['mp3file']
                 '''
                 if cover and allowed_extensions_cover(cover.filename):
                     pass
@@ -96,13 +99,13 @@ class APIServer:
                     raise Exception('Invalid file type for track!')
                 '''
                 #Something happens here
-                print(cover.filename)
-                #print(cover.temporary_file_path)
-                print(os.path.join(upload_folder,cover.filename))
-                cover.save(os.path.join(upload_folder,cover.filename))
-                imagepath = upload_folder+"/"+cover.filename
+
                 uploader=FileUploader()
-                uploader.uploaderimg(os.path.join(upload_folder,cover.filename),cover)
+
+                cover.save(os.path.join(upload_folder,cover.filename))
+                coverurl = uploader.uploaderimg(os.path.join(upload_folder,cover.filename),cover)
+                mp3file.save(os.path.join(upload_folder,mp3file.filename))
+                mp3fileurl = uploader.uploadertrack(os.path.join(upload_folder,mp3file.filename),mp3file)
                 
                 response_msg=jsonify({"status":"200 ok","message":"success"}),200
                 return response_msg 
