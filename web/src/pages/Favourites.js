128b7d16e8a62289741195dfbd5e67a7e8d50398
avinash-vk <avinash2000vk@gmail.com>
2021-04-02 16:20:09 +0530

    added favourites

--- web/src/pages/Favourites.js
+++ web/src/pages/Favourites.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+import SongList from '../components/SongList';
+
+const useStyles = makeStyles(()=>({
+    root:{
+        padding:'4%',
+        paddingTop:"2%",
+        paddingBottom:'1%',
+        marginTop:0,   
+    }
+}))
+
 const Favourites = () => {
+    const classes = useStyles();
     return (
-        <div>
-            This page lists out songs the user favourites
-        </div>
+        <div style = {{display:"flex",flexDirection:'column', maxWidth:'100%'}} direction="row">
+        
+        <Grid item container direction="row" className={classes.root}>
+                <Grid item>
+                    <h2 className={classes.sectionTitle}>
+                        Favourites
+                    </h2>
+                </Grid>
+                <Grid item className={classes.horizontal}>
+                    <SongList />
+                </Grid>
+        </Grid>
+    </div>
     )
 }
 
