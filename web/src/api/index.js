96d7db00f870167b6979054cd5b68c5a825078de
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2021-04-03 15:25:17 +0530

    playlist integrate

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import URLS from './urls';
+
+const Playlists = {
+    createPlaylist : async (params) => (await axios.post(URLS.createPlaylist(),params)).data,
+    addPlaylistToUser : async (params) => (await axios.post(URLS.addPlaylistToUser(),params)).data,
+}
+
 let API;
 export default API = {
-
+    ...Playlists
 }
--- web/src/api/urls.js
+++ web/src/api/urls.js
@@ -1,5 +1,9 @@
-const ENDPOINT = "<Fill in endpoint>"
+const ENDPOINT = "http://localhost:5000"
 let URLS;
+const playlistURL = {
+    createPlaylist:()=>`${ENDPOINT}/playlist/create`,
+    addPlaylistToUser:()=>`${ENDPOINT}/user/playlists`
+}
 export default URLS = {
-    
+    ...playlistURL
 }
