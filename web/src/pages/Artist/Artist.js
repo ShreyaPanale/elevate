63842a85d3c7504a892fc705d0f81bb79db3ae0e
shreyapanale <shreya.panale@gmail.com>
2021-04-04 00:12:54 +0530

    Close button for Artist songs page

--- web/src/pages/Artist/Artist.js
+++ web/src/pages/Artist/Artist.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopBar from '../../components/TopBar';
 
-import { Grid, Avatar } from '@material-ui/core';
+import { Button, Grid, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
 import { usePlayer } from '../../webplayer/provider';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
 
 import ArtistSongList from '../../components/ArtistSongList'
 const useStyles = makeStyles(()=>({
@@ -29,33 +31,46 @@ const Artist = () => {
     const artistList = getArtists()
     const artist = artistList.filter(artist => artist.aid == id)[0];
     console.log(artist);
+
+    const history = useHistory();
+    const goBack = () => {
+        history.goBack()
+      }
+    
     return (
         <Grid container direction="row">
             <Grid item xs={12} >
                 <TopBar placeholder = {"Search for songs or artists"} />
             </Grid>
             <div className={classes.root}>
-                    <div style={{display:"inline-flex",alignSelf:'start'}}>
-                        <h1 style={{
-                            alignItems:'center',
-                            display:'flex',
-                            justifyContent:'center'
-                        }}>
-                        <Avatar
-                                style={{
-                                    width:60,
-                                    height:60,
-                                    borderRadius:100,
-                                    display:'inline-flex',
-                                    marginRight:20
-                                }} 
-                                src={artist.artistProfile}
-                            /> {artist.aname}
-                        </h1>
-                    </div>
-                    <div item xs={12} style={{}}>
-                        <ArtistSongList tracks={trackList}/>
-                    </div>
+                <div style = {{display:"flex",flexDirection:'column', width:'100%'}} direction="row">
+                    <Grid item container direction="row" className={classes.root}>
+                        <Grid container>
+                            <Grid item xs={6}> 
+                                <h1 style={{
+                                    alignItems:'center',
+                                    display:'flex',
+                                }}>
+                                <Avatar style={{
+                                            width:60,
+                                            height:60,
+                                            borderRadius:100,
+                                            display:'inline-flex',
+                                            marginRight:20
+                                        }} 
+                                        src={artist.artistProfile}
+                                />
+                                {artist.aname}</h1>
+                            </Grid>
+                            <Grid item xs={6} style = {{textAlign: 'right'}}> 
+                                <Button onClick={goBack}><h2  style = {{color:"#EF757D", textTransform:'none'}}>Close</h2></Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </div>
+                <div item xs={12} style={{}}>
+                    <ArtistSongList tracks={trackList}/>
+                </div>
             </div>
         </Grid>
     )
