e4767671539f14d4395443bd76b36faa54eab733
avinash-vk <avinash2000vk@gmail.com>
2021-04-07 11:55:14 +0530

    added firebase admin config

--- backend/firebase/config.py
+++ backend/firebase/config.py
@@ -1,8 +1,11 @@
 import os
 import firebase_admin
 from firebase_admin import credentials
+import json
 
-cred = credentials.Certificate(os.getenv("CREDS_PATH"))
+service_account = json.loads(os.getenv("GOOGLE_CREDS")) if os.getenv("APP_ENV") == "PRODUCTION" else os.getenv("CREDS_PATH")
+cred = credentials.Certificate(service_account)
+
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'elevate-25d94.appspot.com'
 })
